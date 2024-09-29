950b5a9754479cd4bbdc919fc97e2b7a14f24229
T3CNEO <neoshon421@gmail.com>
2024-09-28 20:44:30 -0400

    moved files

--- Assets/drag.cs
+++ Assets/drag.cs
@@ -9,8 +9,14 @@ public class drag : MonoBehaviour
     public bool shmove = false;
 
     Vector2 pos1 = new Vector2(-389.6421f, 113.2f);
+    Vector2 pos2 = new Vector2(268, 113.2f);
+    Vector2 pos3 = new Vector2(-389.6421f, -162.47f);
+    Vector2 pos4 = new Vector2(268, -162.47f);
     public gameFunctions gamer;
     public bool imIn1;
+    public bool imIn2;
+    public bool imIn3;
+    public bool imIn4;
 
     Vector2 startingPos;
 
@@ -20,7 +26,10 @@ public class drag : MonoBehaviour
     }
     public void MoveAround(BaseEventData data)
     {
-        PointerEventData pointerData = (PointerEventData)data;
+        if (gamer.started == false && imIn1 == false && imIn2 == false && imIn3 == false && imIn4 == false)
+        {
+
+            PointerEventData pointerData = (PointerEventData)data;
 
             shmove = false;
             Vector2 position;
@@ -30,15 +39,17 @@ public class drag : MonoBehaviour
                 canvas.worldCamera,
                 out position);
             transform.position = canvas.transform.TransformPoint(position);
-
-        Debug.Log(pointerData.position.y);
+        }
     }
 
     public void Update()
     {
         if (Input.GetMouseButtonUp(0))
         {
-            if ((Mathf.Abs(transform.localPosition.x - pos1.x) > 0.1f) || (Mathf.Abs(transform.localPosition.y - pos1.y) > 0.1f))
+            if (((Mathf.Abs(transform.localPosition.x - pos1.x) > 0.1f) || (Mathf.Abs(transform.localPosition.y - pos1.y) > 0.1f)) &&
+                ((Mathf.Abs(transform.localPosition.x - pos2.x) > 0.1f) || (Mathf.Abs(transform.localPosition.y - pos2.y) > 0.1f)) &&
+                ((Mathf.Abs(transform.localPosition.x - pos3.x) > 0.1f) || (Mathf.Abs(transform.localPosition.y - pos3.y) > 0.1f)) &&
+                ((Mathf.Abs(transform.localPosition.x - pos4.x) > 0.1f) || (Mathf.Abs(transform.localPosition.y - pos4.y) > 0.1f)))
                 shmove = true;
           
             if (shmove == true && (Mathf.Abs(transform.localPosition.x - pos1.x) < 100) && (Mathf.Abs(transform.localPosition.y - pos1.y) < 35))
@@ -47,17 +58,56 @@ public class drag : MonoBehaviour
                 {
                     if (gamer.options[i] != this.gameObject && gamer.options[i].GetComponent<drag>().imIn1 == true)
                         gamer.options[i].transform.localPosition = gamer.options[i].transform.localPosition + new Vector3(0, -50, 0);
-                        gamer.options[i].GetComponent<drag>().shmove = true;
-                        gamer.options[i].GetComponent<drag>().imIn1 = false;
+                    gamer.options[i].GetComponent<drag>().shmove = true;
+                    gamer.options[i].GetComponent<drag>().imIn1 = false;
                 }
                 gamer.oneIs = this.gameObject.tag;
                 imIn1 = true;
+            }
+            if (shmove == true && (Mathf.Abs(transform.localPosition.x - pos2.x) < 100) && (Mathf.Abs(transform.localPosition.y - pos2.y) < 35))
+            {
+                for (int i = 0; i < gamer.options.Length; i++)
+                {
+                    if (gamer.options[i] != this.gameObject && gamer.options[i].GetComponent<drag>().imIn2 == true)
+                        gamer.options[i].transform.localPosition = gamer.options[i].transform.localPosition + new Vector3(0, -50, 0);
+                    gamer.options[i].GetComponent<drag>().shmove = true;
+                    gamer.options[i].GetComponent<drag>().imIn2 = false;
+                }
+                gamer.twoIs = this.gameObject.tag;
+                imIn2 = true;
+            }
+            if (shmove == true && (Mathf.Abs(transform.localPosition.x - pos3.x) < 100) && (Mathf.Abs(transform.localPosition.y - pos3.y) < 35))
+            {
+                for (int i = 0; i < gamer.options.Length; i++)
+                {
+                    if (gamer.options[i] != this.gameObject && gamer.options[i].GetComponent<drag>().imIn3 == true)
+                        gamer.options[i].transform.localPosition = gamer.options[i].transform.localPosition + new Vector3(0, -50, 0);
+                    gamer.options[i].GetComponent<drag>().shmove = true;
+                    gamer.options[i].GetComponent<drag>().imIn3 = false;
+                }
+                gamer.thrIs = this.gameObject.tag;
+                imIn3 = true;
+            }
+            if (shmove == true && (Mathf.Abs(transform.localPosition.x - pos4.x) < 100) && (Mathf.Abs(transform.localPosition.y - pos4.y) < 35))
+            {
+                for (int i = 0; i < gamer.options.Length; i++)
+                {
+                    if (gamer.options[i] != this.gameObject && gamer.options[i].GetComponent<drag>().imIn4 == true)
+                        gamer.options[i].transform.localPosition = gamer.options[i].transform.localPosition + new Vector3(0, -50, 0);
+                    gamer.options[i].GetComponent<drag>().shmove = true;
+                    gamer.options[i].GetComponent<drag>().imIn4 = false;
+                }
+                gamer.fouIs = this.gameObject.tag;
+                imIn4 = true;
             }
         }
         if (shmove == true)
         {
             Shmove();
-            if (imIn1 == true && (Mathf.Abs(transform.localPosition.x - pos1.x) < 0.1f) || (Mathf.Abs(transform.localPosition.y - pos1.y) < 0.1f))
+            if ((imIn1 == true && (Mathf.Abs(transform.localPosition.x - pos1.x) < 0.05f) || (Mathf.Abs(transform.localPosition.y - pos1.y) < 0.1f)) ||
+                (imIn2 == true && (Mathf.Abs(transform.localPosition.x - pos2.x) < 0.05f) || (Mathf.Abs(transform.localPosition.y - pos2.y) < 0.1f)) ||
+                (imIn3 == true && (Mathf.Abs(transform.localPosition.x - pos3.x) < 0.05f) || (Mathf.Abs(transform.localPosition.y - pos3.y) < 0.1f)) ||
+                (imIn4 == true && (Mathf.Abs(transform.localPosition.x - pos4.x) < 0.05f) || (Mathf.Abs(transform.localPosition.y - pos4.y) < 0.1f)))
                 shmove = false;
         }
     }
@@ -70,6 +120,12 @@ public class drag : MonoBehaviour
     {
         if (imIn1 == true && (Mathf.Abs(transform.localPosition.x - pos1.x) < 100) && (Mathf.Abs(transform.localPosition.y - pos1.y) < 35))
             transform.localPosition = Vector2.Lerp(transform.localPosition, pos1, 4*Time.deltaTime);
+        else if (imIn2 == true && (Mathf.Abs(transform.localPosition.x - pos2.x) < 100) && (Mathf.Abs(transform.localPosition.y - pos2.y) < 35))
+            transform.localPosition = Vector2.Lerp(transform.localPosition, pos2, 4 * Time.deltaTime);
+        else if (imIn3 == true && (Mathf.Abs(transform.localPosition.x - pos3.x) < 100) && (Mathf.Abs(transform.localPosition.y - pos3.y) < 35))
+            transform.localPosition = Vector2.Lerp(transform.localPosition, pos3, 4 * Time.deltaTime);
+        else if (imIn4 == true && (Mathf.Abs(transform.localPosition.x - pos4.x) < 100) && (Mathf.Abs(transform.localPosition.y - pos4.y) < 35))
+            transform.localPosition = Vector2.Lerp(transform.localPosition, pos4, 4 * Time.deltaTime);
         else
             transform.localPosition = Vector2.Lerp(transform.localPosition, startingPos, 4 * Time.deltaTime);
     }
--- Assets/gameFunctions.cs
+++ Assets/gameFunctions.cs
@@ -8,40 +8,193 @@ using UnityEngine.Video;
 
 public class gameFunctions : MonoBehaviour
 {
-    public VideoClip fast1;
-    public VideoClip smart1;
+    [SerializeField] string greenSpeedy;
+    [SerializeField] string greenSmart;
+    [SerializeField] string orangeSpeedy;
+    [SerializeField] string orangeSmart;
+    [SerializeField] string blueSpeedy;
+    [SerializeField] string blueSmart;
+    [SerializeField] string blackSpeedy;
+    [SerializeField] string blackSmart;
 
-    public VideoPlayer[] one;
+    [SerializeField] string youDidIt;
+
+    [SerializeField] GameObject badge;
+
+
+    public VideoPlayer[] vids;
+    public GameObject[] thumbs;
 
     public GameObject[] options;
 
     public string oneIs;
+    public string twoIs;
+    public string thrIs;
+    public string fouIs;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool started;
+    bool twoStarted;
+
+    bool threeOkayToStartSmileFace;
+    bool fourOkayToStartSmileFace;
+    bool endingOkay;
+    bool endingHappened;
+
+    bool thrStarted;
+    bool fouStarted;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (vids[0].isPlaying && endingHappened == false)
+        {
+            started = true;
+        }
+        if (vids[0].isPlaying == false && started == true && twoStarted == false)
+        {
+            if (twoIs != "" && oneIs == "Fast")
+            {
+                string videoPath;
+                if (twoIs == "Smart")
+                {
+                    videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, orangeSmart);
+                    vids[1].url = videoPath;
+                }
+                else if (twoIs == "Fast")
+                {
+                    videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, orangeSpeedy);
+                    vids[1].url = videoPath;
+                }
+                vids[1].Play();
+                thumbs[1].SetActive(false);
+                twoStarted = true;
+            }
+            else
+            {
+                started = false;
+            }
+        }
+        if (vids[1].isPlaying && endingHappened == false)
+            threeOkayToStartSmileFace = true;
+        if (vids[1].isPlaying == false && threeOkayToStartSmileFace == true && thrStarted == false)
+        {
+            if (thrIs != "" && twoIs == "Smart")
+            {
+                threeOkayToStartSmileFace = false;
+                string videoPath;
+                if (thrIs == "Smart")
+                {
+                    videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, blueSmart);
+                    vids[2].url = videoPath;
+                }
+                else if (thrIs == "Fast")
+                {
+                    videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, blueSpeedy);
+                    vids[2].url = videoPath;
+                }
+                vids[2].Play();
+                thumbs[2].SetActive(false);
+                thrStarted = true;
+            }
+            else
+            {
+                started = false;
+                twoStarted = false;
+                threeOkayToStartSmileFace = false;
+            }
+        }
+
+        if (vids[2].isPlaying && endingHappened == false)
+            fourOkayToStartSmileFace = true;
+
+        if (vids[2].isPlaying == false && fourOkayToStartSmileFace == true && fouStarted == false)
+        {
+            if (fouIs != "" && thrIs == "Fast")
+            {
+                fourOkayToStartSmileFace = false;
+                string videoPath;
+                if (thrIs == "Smart")
+                {
+                    videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, blackSmart);
+                    vids[3].url = videoPath;
+                }
+                else if (thrIs == "Fast")
+                {
+                    videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, blackSpeedy);
+                    vids[3].url = videoPath;
+                }
+                vids[3].Play();
+                thumbs[3].SetActive(false);
+                fouStarted = true;
+            }
+            else
+            {
+                started = false;
+                twoStarted = false;
+                thrStarted = false;
+                fourOkayToStartSmileFace = false;
+            }
+        }
+
+        if (vids[3].isPlaying && endingHappened == false)
+            endingOkay = true;
+
+
+        if (vids[3].isPlaying == false && endingOkay == true)
+        {
+            Debug.Log("1");
+            if (fouIs == "Smart")
+            {
+                Debug.Log(2);
+                string videoPath;
+                videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, blackSmart);
+                vids[0].url = videoPath;
+                vids[1].url = videoPath;
+                vids[2].url = videoPath;
+                vids[3].url = videoPath;
+                
+                vids[0].Play();
+                vids[1].Play();
+                vids[2].Play();
+                vids[3].Play();
+                started = false;
+                twoStarted = false;
+                thrStarted = false;
+                fouStarted = false;
+                endingOkay = false;
+                endingHappened = true;
+                badge.SetActive(true);
+            }
+            else
+            {
+                Debug.Log("3");
+                started = false;
+                twoStarted = false;
+                thrStarted = false;
+                fouStarted = false;
+                endingOkay = false;
+            }
+        }
     }
 
     public void PlayVideo()
     {
-        if (oneIs != "")
+        if (oneIs != "" && started == false)
         {
+            string videoPath;
             if (oneIs == "Smart")
             {
-                one[0].clip = smart1;
+                videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, greenSmart);
+                vids[0].url = videoPath;
             }
-            if (oneIs == "Fast")
+            else if (oneIs == "Fast")
             {
-                one[0].clip = fast1;
+                videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, greenSpeedy);
+                vids[0].url = videoPath;
             }
-            one[0].Play();
+            vids[0].Play();
+            thumbs[0].SetActive(false);
+            endingHappened = false;
         }
     }
 }
